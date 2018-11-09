60d7c0be36f7d5dd0102d8ba394c33cc93f8b7e3
luciliarosa <lucilia.rosa@fatec.sp.gov.br>
2018-11-09 15:50:06 -0200

    exerciciosjavascritp

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+let numero = Number (prompt('Digite um número: '));
+let i = 1;
+let resultado = 0;
+
+for (i = 0 && i <11 && i++) {
+	resultado = numero * i;
+}
+
+alert(`${numero} x ${i} = ${resultado}`);
+
